57e2abd7851779a62612107b30d977a77654b505
Felix Nüsse <felix.nuesse@t-online.de>
2022-01-06 20:43:22 +0100

    remove fab

--- app/src/main/java/com/polar/nextcloudservices/MainActivity.java
+++ app/src/main/java/com/polar/nextcloudservices/MainActivity.java
@@ -58,13 +58,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
